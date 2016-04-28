ba766e236a15d98fb384bf1668b654408ad5abb8
黄根硕，原名黄大根，现在读硕士 <huanghuoyu@gmail.com>
2016-04-29 06:22:28 +0800

    csv read function

---
+++ Assets/test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	public TextAsset csvFile;
+	// Use this for initialization
+	void Start () {
+		string[] records = csvFile.text.Split ('\n');
+		foreach (string record in records){
+			string[] attrs = csvFile.text.Split (',');
+			foreach (string attr in attrs){
+				Debug.Log(attr);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
